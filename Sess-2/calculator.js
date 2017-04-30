1863cb660558097a05ff2a787955f998663e3369
Kaido Kukk <kaido.kukk@gmail.com>
2017-04-30 19:32:25 +0300

    Lisatud lehele juurde kalkulaator ringi pindala leidmiseks

--- Sess-2/calculator.js
+++ Sess-2/calculator.js
@@ -16,8 +16,24 @@ class Calculator {
 	calculate() {
 		this.responseArea.innerHTML = parseFloat(this.box.value) * 2.54;
 	}
+}
 
-	algus() {
-
+class Calculator2 {
+	constructor(layerName) {
+		this.layer = document.getElementById(layerName);
+		window[layerName + "_calculator"] = this;
+		
+		this.layer.innerHTML = 
+			"Raadius: <input type='text' id='" + layerName + "_box' /> "+
+			"<input type='button' value='Ringi pindala' "+
+			"onClick='" + layerName + "_calculator.calculate();' /> "+
+			"<div id='" + layerName + "_vastus'></div>";
+		
+		this.box = document.getElementById(layerName + "_box");
+		this.responseArea = document.getElementById(layerName + "_vastus");
+	}
+	
+	calculate() {
+		this.responseArea.innerHTML = (Math.pow(parseFloat(this.box.value), 2) * Math.PI).toFixed(2);
 	}
 }
