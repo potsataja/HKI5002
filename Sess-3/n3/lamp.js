a7656d9557309d524a9098c72506f7f4028cc030
Kaido Kukk <kaido.kukk@gmail.com>
2017-05-07 21:45:11 +0300

    Lisatud lampidele nupud

--- Sess-3/n3/lamp.js
+++ Sess-3/n3/lamp.js
@@ -1,6 +1,18 @@
 class Lamp {
-    constructor() {
+    constructor(kihinimi) {
         this.seisund = true;
+        this.kiht = document.getElementById(kihinimi);
+        window[kihinimi + "_lamp"] = this;
+
+        this.kiht.innerHTML =
+            "<p>" + kihinimi + ": "
+            + "<input type='button' value='Vaheta' "
+            + " onClick='" + kihinimi + "_lamp.vahetaSeisund();' />"
+            + "<input type='button' value='Küsi' "
+            + " onClick='" + kihinimi + "_lamp.näitaSeisund();' /></p>";
+
+        this.vastusekiht = document.getElementById(kihinimi + "_vastus");
+        this.kihinimi = kihinimi;
     }
 
     kasSees() {
@@ -12,10 +24,10 @@ class Lamp {
     }
 
     seisundTekstina() {
-        if(this.seisund) {
+        if (this.seisund) {
             return "põleb";
         }
-        else if(!this.seisund) {
+        else if (!this.seisund) {
             return "ei põle";
         }
         else {
@@ -24,14 +36,20 @@ class Lamp {
     }
 
     vahetaSeisund() {
-        if(this.seisund) {
+        if (this.seisund) {
             this.seisund = false;
         }
-        else if(!this.seisund) {
+        else if (!this.seisund) {
             this.seisund = true;
         }
         else {
             //return "viga";
         }
+
+        //console.log(this.kihinimi + ": " + this.seisundTekstina());
+    }
+
+    näitaSeisund() {
+        console.log(this.kihinimi + ": " + this.seisundTekstina());
     }
 }
